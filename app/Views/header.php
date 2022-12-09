13d9f8696d43857add500a813d51cec1eff5c72f
odebagati <bagatikusuma@gmail.com>
2022-12-09 15:29:14 +0800

    add navbar

--- app/Views/header.php
+++ app/Views/header.php
@@ -9,3 +9,25 @@
     <title>Morato Bali</title>
 </head>
 <body>
+
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="<?= base_url(); ?>/assets/img/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                Bootstrap
+            </a>
+            <div class="collapse navbar-collapse justify-content-end text-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item me-2">
+                        <a class="nav-link active" aria-current="page" href="<?= base_url(); ?>">Process</a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link active" href="#">Features</a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link active" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
